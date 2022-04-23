50474e3ec70631291f9c593d8d8a375f828dd6bf
Eric Chen <eric@spotlight.health>
2022-04-23 14:29:39 -0700

    styling

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,43 @@
+<svelte:head>
+</svelte:head>
+
+<script lang="ts">
+    import "$lib/global.css";
+
+    let isOpen = false;
+
+    function handleUpdate(event: any) {
+        isOpen = event.detail.isOpen
+    };
+</script>
+
+<div class="navbar bg-base-100">
+    <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/">Home</a></li>
+            <li><a href="/coffees">Coffees</a></li>
+            <li><a href="/brewing">Brewing</a></li>
+            <li><a href="/classes">Tastings & Classes</a></li>
+            <li><a href="/community">Community</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+    <div class="flex-1">
+        <a href="/" class="btn btn-ghost normal-case text-xl">DRINK THE DIRT</a>
+    </div>
+    <div class="hidden lg:flex navbar-center">
+        <ul class="menu menu-horizontal p-0">
+            <li><a href="/">Home</a></li>
+            <li><a href="/coffees">Coffees</a></li>
+            <li><a href="/brewing">Brewing</a></li>
+            <li><a href="/classes">Tastings & Classes</a></li>
+            <li><a href="/community">Community</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+</div>
+
+<slot></slot>
