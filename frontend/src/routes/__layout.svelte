560d918706687f2e4151f7a35662df44faf5615d
Eric Chen <eric@spotlight.health>
2022-04-23 15:33:14 -0700

    container padding

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -15,7 +15,7 @@
     setClient(apolloClient);
 </script>
 
-<div class="navbar bg-base-100">
+<div class="navbar bg-base-200">
     <div class="dropdown">
         <label tabindex="0" class="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -44,6 +44,6 @@
     </div>
 </div>
 
-<div class="prose">
+<div class="container px-4 py-4 prose">
     <slot></slot>
 </div>
