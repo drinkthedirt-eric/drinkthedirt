f6af554724859a71358c31ab488f29bf0ba454a7
Eric Chen <eric@spotlight.health>
2022-04-23 19:18:19 -0700

    missed some little styling

--- frontend/src/routes/classes.svelte
+++ frontend/src/routes/classes.svelte
@@ -5,5 +5,5 @@
 <script lang="ts">
 </script>
 
-<div class="font-bold text-4xl">Tastings & Classes</div>
+<div class="font-bold text-4xl pb-2">Tastings & Classes</div>
 <div>Join us live in Seattle or over the web for personalized tastings or classes. We can dive deep into tasting, help you improve your brew, or just share some coffees that we enjoy.</div>
