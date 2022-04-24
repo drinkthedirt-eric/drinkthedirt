ad75d9cf53e09ceec0758062a66cfe65d1091c52
Luke Liu <103903823+lukedtd@users.noreply.github.com>
2022-04-23 17:17:07 -0700

    coffee detail page wip

--- frontend/src/routes/coffees/[id].svelte
+++ frontend/src/routes/coffees/[id].svelte
@@ -4,14 +4,15 @@
     link: "https://ondaorigins.com/",
   };
   const coffee = {
-    name: "Umoja",
+    name: "Umoja / Congo",
     roaster: roaster,
     country: "Democratic Republic of Congo",
     location: "Kalehe Territory, South Kivu Province",
     producer: "Solidarité Pour La Promotion des Actions Café et le Développement Intégral",
-    varietals: ["SL-34"],
     process: "Washed",
+    varietals: ["SL-34"],
     roasterTastingNotes: ["kumquat", "white pepper", "cantaloupe"],
+    roasterDescription: "This certified fairtrade and organic cooperative provides stability for its members, for whom coffee is a key source of income and helps promote their rights.",
     flavorCategories: ["Fruity"],
     sweetness: "High",
     body: "Medium",
@@ -20,8 +21,40 @@
   };
 </script>
 
-<div class="flex justify-center">
-  <div class="card max-w-7xl">
-    <p>{coffee.name}</p>
+<div class="flex justify-center font-light leading-tight">
+  <div class="card w-full max-w-7xl bg-base-200 px-4 py-4">
+    <div class="grid grid-cols-3 gap-4">
+      <div class="col-span-1">
+        <div class="text-xl font-light leading-none">{coffee.roaster.name}</div>
+        <div class="text-4xl font-thin leading-none">{coffee.name}</div>
+        <div class="mt-2 text-sm">
+          <span>$18.00</span>
+          <span class="font-extralight">- 12 oz / 340 g</span>
+        </div>
+      </div>
+      <div class="col-span-2">
+        <div class="pb-2">
+          <div class="font-semibold">origin</div>
+          <div>{coffee.location}, {coffee.country}</div>
+        </div>
+        <div class="pb-2">
+          <div class="font-semibold">producer</div>
+          <div>{coffee.producer}</div>
+        </div>
+        <div class="pb-2">
+          <div class="font-semibold">process</div>
+          <div>{coffee.process}</div>
+        </div>
+        <div class="pb-2">
+          <div class="font-semibold">varietal</div>
+          <div>{coffee.varietals.join(", ")}</div>
+        </div>
+        <div class="pb-2">
+          <div class="font-semibold">from the roaster</div>
+          <div>{coffee.roasterTastingNotes.join(", ")}</div>
+          <div class="mt-2">{coffee.roasterDescription}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
