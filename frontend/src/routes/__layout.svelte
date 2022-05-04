0f3b5dcb7c6f3461ca69379095c35440b648f281
Eric Chen <eric@spotlight.health>
2022-05-04 11:46:43 -0700

    navbar responsiveness

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -29,7 +29,7 @@
 
 <div class="navbar bg-base-200">
     <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <label tabindex="0" class="btn btn-ghost md:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
@@ -43,7 +43,7 @@
     <div class="flex-1">
         <a href="/" class="btn btn-ghost normal-case text-xl">DRINK THE DIRT</a>
     </div>
-    <div class="hidden lg:flex navbar-center">
+    <div class="hidden md:flex navbar-center">
         <ul class="menu menu-horizontal p-0">
             <li><a href="/">Home</a></li>
             <li><a href="/coffees">Coffees</a></li>
